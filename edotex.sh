ede50cefd4710d9eff2a2ac87efc5eb382b88f16
Benjamin Benno Falkner <benno.falkner@gmail.com>
2017-09-21 20:00:05 +0200

    Linux SH added

---
+++ edotex.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Edo.TeX 2016"
+TEXINPUTS=$(pwd)/texmf/tex/edo export TEXINPUTS
+TEXFORMAT=$(pwd)/texmf/tex/edo export TEXFORMAT
+ENCFONTS=$(pwd)/texmf/fonts/enc export ENCFONTS
+TFMFONTS=$(pwd)/texmf/fonts/tfm export TFMFONTS
+TEXFONTMAPS=$(pwd)/texmf/fonts/map export TEXFONTMAPS
+T1FONTS=$(pwd)/texmf/fonts/type1 export T1FONTS
+
+echo $TFMFONTS
+
+pdftex --ini --recorder --file-line-error edo.pdf.tex
+mv -f edo.pdf.fmt ./texmf/tex/edo/
+
+echo "Edo.pdf.tex build" 
+
+cat <<EOF > ./bin/edotex
+echo "Edo.TeX 2016"
+TEXINPUTS=$(pwd)/texmf/tex/edo export TEXINPUTS
+TEXFORMAT=$(pwd)/texmf/tex/edo export TEXFORMAT
+ENCFONTS=$(pwd)/texmf/fonts/enc export ENCFONTS
+TFMFONTS=$(pwd)/texmf/fonts/tfm export TFMFONTS
+TEXFONTMAPS=$(pwd)/texmf/fonts/map export TEXFONTMAPS
+T1FONTS=$(pwd)/texmf/fonts/type1 export T1FONTS
+pdftex -fmt $(pwd)/texmf/tex/edo/edo.pdf $1
+EOF
+chmod u+x ./bin/edotex
+echo "Add $(pwd)/bin to your PATH" 
