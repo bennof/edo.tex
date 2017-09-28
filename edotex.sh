6add33d3c8adb2d9bd7d5fbdd023a5ba55ec7dbf
Benjamin Benno Falkner <benno.falkner@gmail.com>
2017-09-28 19:08:33 +0200

    added computer modern font

--- edotex.sh
+++ edotex.sh
@@ -16,13 +16,13 @@ echo "Edo.pdf.tex build"
 
 cat <<EOF > ./bin/edotex
 echo "Edo.TeX 2016"
-TEXINPUTS=$(pwd)/texmf/tex/edo export TEXINPUTS
+TEXINPUTS=.:$(pwd)/texmf/tex/edo export TEXINPUTS
 TEXFORMAT=$(pwd)/texmf/tex/edo export TEXFORMAT
 ENCFONTS=$(pwd)/texmf/fonts/enc export ENCFONTS
 TFMFONTS=$(pwd)/texmf/fonts/tfm export TFMFONTS
 TEXFONTMAPS=$(pwd)/texmf/fonts/map export TEXFONTMAPS
 T1FONTS=$(pwd)/texmf/fonts/type1 export T1FONTS
-pdftex -fmt $(pwd)/texmf/tex/edo/edo.pdf $1
+pdftex -fmt $(pwd)/texmf/tex/edo/edo.pdf \$@
 EOF
 chmod u+x ./bin/edotex
 echo "Add $(pwd)/bin to your PATH" 
